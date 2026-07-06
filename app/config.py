430e5c64c4c5eade9bf4b20737fe43b04336dbaa
Colm Nolan <182516888+ColmN-Dev@users.noreply.github.com>
2026-07-06 22:54:43 +0100

    chore(config): update database configuration

--- app/config.py
+++ app/config.py
@@ -1,9 +1,12 @@
-import os 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Configuration class for the Flask application. It sets up the secret key and database URI, using environment variables if available, or default values otherwise.
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY') or 'default_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') or 'postgresql:///default.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     
     # Disable SQLAlchemy event system to save resources, as it is not needed for this application.
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
